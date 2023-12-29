2349e7c4a5f197c075c36b36f9f20eda84634782
Bartek <bmroczkowski@icloud.com>
2023-12-29 13:18:06 +0100

    Chaniging colors

--- main.py
+++ main.py
@@ -72,20 +72,20 @@ class Calculator(tk.Frame):
 
     MAX_LENGTH = 11
 
+    SYMBOLS = {"( )": 10, ".": 11, "%": 12, "\u00B2": 13, "^": 14, "+": 15, "-": 16, "*": 17, "/": 18, "B": 19, "": 20, "√": 21, "3.14": 22, "t": 24, "=": 25}
+
     def __init__(self, master):
         super().__init__(master)
         self.colors = self.load_colors()
 
-        self.config = None # None | "bg" | "fg"
+        self.config = None  # None | "bg" | "fg"
 
         self.grid()
         self.create_widget()
 
-    def load_colors(self, reset: bool = False) -> dict:
-        colors = {"bg": self.BG_DEFAULT, "fg": self.FG_DEFAULT}
-
+    def load_colors(self, colors={"bg": BG_DEFAULT, "fg": FG_DEFAULT}, set: bool = False) -> dict:
         try:
-            if reset:
+            if set:
                 raise
 
             with open("./config.json", "r", encoding="UTF-8") as f:
@@ -117,6 +117,8 @@ class Calculator(tk.Frame):
             "bd": 0,
             "bg": self.colors["bg"][x],
             "fg": self.colors["fg"][x],
+            "activebackground": self.colors["bg"][x],
+            "activeforeground": self.colors["fg"][x],
         }
 
         self.grid(column=0, sticky="NESW")
@@ -136,9 +138,7 @@ class Calculator(tk.Frame):
         self.rowconfigure(6, pad=1)
         self.rowconfigure(7)
 
-        self.text = tk.Label(
-            self, text="0", font=FONT_L, height=self.HEIGHT, bg=self.colors["bg"][24], fg=self.colors["fg"][24], anchor="e"
-        )
+        self.text = tk.Label(self, text="0", font=FONT_L, height=self.HEIGHT, bg=self.colors["bg"][24], fg=self.colors["fg"][24], anchor="e")
         self.text.grid(row=1, columnspan=4, sticky="we")
 
         self.text_2 = tk.Label(self, text="", font=FONT_S, height=1, bg=self.colors["bg"][24], fg=self.colors["fg"][24], anchor="e")
@@ -199,11 +199,10 @@ class Calculator(tk.Frame):
                 self.config = "fg"
                 self.set_text(self.text, "FG Setup")
 
-            case "t", "bg" | "fg":
-                char = "l"
             case "d", "bg" | "fg":
-                self.colors = self.load_colors(True)
+                self.colors = self.load_colors(set=True)
                 self.create_widget()
+                self.set_text(self.text, "BG Setup" if self.config == "bg" else "FG Setup")
 
             case "+" | "-" | "*" | "/" | "%" | "^", _:
                 char = f" {char} "
@@ -211,6 +210,9 @@ class Calculator(tk.Frame):
                 char = " ^ "
             case "p", _:
                 char = "3.14"
+
+        if char.strip() not in self.SYMBOLS or (char.isdigit() and int(char) not in range(0, 10)):
+            return
 
         self.click(char)
 
@@ -248,9 +250,7 @@ class Calculator(tk.Frame):
         l3 = l2[:-1]
         length = len(l2)
         last_char = l2[length - 1]
-        self.text["text"] = (
-            l3.replace("+", " + ").replace("*", " * ").replace("-", " - ").replace("/", " / ").replace("^", " ^ ").replace("√", "√ ")
-        )
+        self.text["text"] = l3.replace("+", " + ").replace("*", " * ").replace("-", " - ").replace("/", " / ").replace("^", " ^ ").replace("√", "√ ")
         if self.text["text"] == "":  # Never blank label text
             self.text["text"] = "0"
             self.text_2["text"] = ""
@@ -283,49 +283,27 @@ class Calculator(tk.Frame):
         else:
             self.text["text"] += btn_id
 
+    def change_color(self, btn_id):
+        btn_id = self.SYMBOLS[btn_id] if btn_id in self.SYMBOLS else int(btn_id)
+        color = colorchooser.askcolor()
+
+        if color[1] is None:
+            return
+
+        color = color[1].strip()
+
+        del self.colors[self.config][btn_id]
+        self.colors[self.config].insert(btn_id, color)
+
+        self.create_widget()
+        self.text["text"] = "BG Setup" if self.config == "bg" else "FG Setup"
+
+        self.load_colors(self.colors, True)
+
     def click(self, btn_id):
         if self.config in ["bg", "fg"]:
-            if btn_id == "3.14":
-                btn_id = "22"
-            btn_id = (
-                btn_id.replace("( )", "10")
-                .replace(".", "11")
-                .replace("l", "24")
-                .replace(" % ", "12")
-                .replace("√ ", "21")
-                .replace("\u00B2", "13")
-                .replace(" ^ ", "14")
-                .replace(" + ", "15")
-                .replace("=", "25")
-                .replace(" - ", "16")
-                .replace(" * ", "17")
-                .replace("B", "18")
-                .replace(" / ", "19")
-                .replace(" ", "20")
-                .replace("t", "21")
-            )
-            if "c" in dict4:
-                self.text["text"] = "FontColorSetup"
-                try:
-                    my_color = colorchooser.askcolor()
-                    my_color = my_color[1].strip()
-                    # CHANGE COLOR LIST
-                    self.create_widget()
-                    self.text["text"] = "FontColorSetup"
-                except AttributeError:
-                    return
-            else:
-                self.text["text"] = "ColorSetup"
-                try:
-                    my_color = colorchooser.askcolor()
-                    my_color = my_color[1].strip()
-                    # CHANGE COLOR LIST
-                    self.create_widget()
-                    self.text["text"] = "ColorSetup"
-                except AttributeError:
-                    return
-
-            # SAVE COLORS
+            self.change_color(btn_id.strip())
+            return
 
         if btn_id == " ":  # All Clear
             self.AC()
